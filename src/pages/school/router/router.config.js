07673abdf8c3f59e39c1c948eb512067083a8d4c
zhangliGit <674937076@qq.com>
2020-03-02 11:33:55 +0800

    代码优化

---
+++ src/pages/school/router/router.config.js
@@ -0,0 +1,47 @@
+// eslint-disable-next-line
+import { BasicLayout } from '../layouts'
+import system from './system'
+const Home = resolve => require(['../views/Home.vue'], resolve)
+
+export const asyncRouterMap = [
+  {
+    path: '/',
+    name: 'index',
+    component: BasicLayout,
+    meta: { title: '系统首页' },
+    redirect: '/home',
+    children: [
+      {
+        path: '/home',
+        name: 'home',
+        component: Home,
+        meta: {
+          title: '系统首页',
+          icon: 'home'
+        }
+      },
+      system
+    ]
+  },
+  {
+    path: '*',
+    redirect: '/404',
+    hidden: true
+  }
+]
+
+/**
+ * 基础路由
+ * @type { *[] }
+ */
+const constantRouterMap = [
+  // {
+  //   path: '',
+  //   name: 'login',
+  //   component: () => import(/* webpackChunkName: "login" */ '../views/user/Login')
+  // }
+]
+
+const RouterMap = [...constantRouterMap, ...asyncRouterMap]
+
+export default RouterMap
---
+++ src/pages/school/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home page-layout">学校端</div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {},
+  data() {
+    return {}
+  },
+  mounted() {},
+  methods: {}
+}
+</script>
+<style lang="less" scoped></style>
