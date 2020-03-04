13110addad8783c02be9467c20c9c4d0143b930c
unknown <824876766@qq.com>
2020-03-04 09:35:46 +0800

    代码拉取

---
+++ src/pages/organize/views/daily/Daily.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="page-layout">
+    疫情日报
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Daily',
+  components: {},
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+<style lang="less" scoped></style>
---
+++ src/pages/organize/views/healthFile/HealthFile.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="page-layout">
+    健康档案
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HealthFile',
+  components: {},
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+<style lang="less" scoped></style>
