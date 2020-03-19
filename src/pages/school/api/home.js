43f47b340eb244540c6866698f4e67229336d977
tiancheng <1005265162@qq.com>
2020-03-19 20:23:09 +0800

    优化

--- src/pages/school/api/home.js
+++ src/pages/school/api/home.js
@@ -9,9 +9,15 @@ const homeApi = {
   getIndex: 'http://192.168.2.247:3000/mock/40/getTable#post', // 获取数据列表
   getDetail: 'http://192.168.2.247:3000/mock/40/detail#get', // 获取详情
   getUnReport: '/school/unReport/list#get', // 未上报名单
-  getDaily: '/school/daily/num#get' // 疫情日报数量
+  getDaily: '/school/daily/num#get', // 疫情日报数量
+  getReport: '/school/reportinfo/getReportInfoList#post', // 获取上报信息
+  getInfoDoctor: '/school/schooldoctorinfo/getSchoolDoctorList#postQuery', // 获取校医列表
+  getDailyData: '/index/page/getEpidemicData#postQuery', // 获取首页疫情日报数据
+  getFeverAndHealth: '/index/page/getFeverAndHealthData#postQuery', // 获取首页发热和健康数
+  getNoReport: '/index/page/getNoReportUserData#postQuery', // 获取首页未上报人数统计
+  getSymptomsUser: '/index/page/getSymptomsUserData#postQuery' // 获取首页个症状人数统计
 }
 for (const val in homeApi) {
-  homeApi[val] = `${hostEnv}${homeApi[val]}`
+  homeApi[val] = `/admin${homeApi[val]}`
 }
 export default homeApi
